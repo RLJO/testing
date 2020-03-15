87f4b61197b0cadac894c78e9ccbfc666efa665b
Hammam <GM2@royalline.Gold>
2020-03-15 10:17:49 +0200

    5555

--- clv_person_sel_jcafb/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import abstract_row
-from . import person_sel_age_group
-from . import person_sel_district
-from . import person_sel_group
-from . import person_sel_summary
